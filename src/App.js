f603a425c0d0ff3133ce049051d9966d7bbde08f
sarahishamsaied <sarahishamsaied@gmail.com>
2022-03-12 20:16:40 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Fragment} from 'react'
+import NavBar from './Components/Navbar/NavBar';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Fragment>
+    <NavBar/>
+    <h1>Hii</h1>
+    <h1>Heelo</h1>
+  </Fragment>
 }
 
 export default App;
